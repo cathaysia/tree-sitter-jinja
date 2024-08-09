ea91748e01fbe256847f6eff05f17754b57872f7
loongtao.zhang <loongtao.zhang@outlook.com>
2024-08-09 18:23:30 +0800

    feat: support with
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- grammar.js
+++ grammar.js
@@ -1,4 +1,4 @@
-const { anySep1, commaSep } = require('./common/common')
+const { anySep1, commaSep, commaSep1, anySep } = require('./common/common')
 
 const PREC = {
   LOGICAL_OR: 1,
@@ -21,46 +21,116 @@ module.exports = grammar({
 
   rules: {
     source: $ => repeat($.definition),
-    definition: $ =>
-      choice($.control, $.render_expression, $.statement, $._word),
+    definition: $ => choice($.control, $.render_expression, $._word),
     control: $ => seq(choice('{%', '{%-'), $.statement, choice('-%}', '%}')),
     render_expression: $ =>
-      seq(choice('{{', '{{-'), $.expression, choice('}}', '-}}')),
-    statement: $ =>
-      choice(
-        'endfor',
-        'endif',
-        seq('elif', $.expression),
-        seq('filter', $.expression),
-        'endfilter',
-        'else',
-        $.set_expression,
-        $.for_expression,
-        $.if_expression,
+      seq(
+        choice('{{', '{{-'),
+        $.expression,
+        optional($.inline_if_expression),
+        choice('}}', '-}}'),
       ),
-    set_expression: $ => seq('set', $.identifier, '=', $.expression),
-    for_expression: $ => seq('for', $.identifier, 'in', $.expression),
+    statement: $ =>
+      seq(
+        choice(
+          'endfor',
+          seq('elif', $.expression),
+          'else',
+          'endif',
+          'endblock',
+          'endwith',
+          'endfilter',
+          'endmacro',
+          'endcall',
+          'endset',
+          'endtrans',
+          'continue',
+          'break',
+          'debug',
+          'endautoescape',
+          seq('do', $.expression),
+          $.include_statement,
+          $.import_statement,
+          $.set_statement,
+          $.for_expression,
+          $.if_expression,
+          $.with_statement,
+          $.call_statement,
+          $.set_statement,
+          $.extends_statement,
+          $.macro_statement,
+          $.filter_statement,
+          $.block_statement,
+          $.pluralize_statement,
+          $.trans_statement,
+          $.autoescape_statement,
+        ),
+        optional($.inline_if_expression),
+      ),
+    autoescape_statement: $ => seq('autoescape', optional($.boolean_literal)),
+    trans_statement: $ =>
+      seq('trans', commaSep(choice($.identifier, $.assignment_expression))),
+    pluralize_statement: $ => seq('pluralize', optional($.identifier)),
+    inline_if_expression: $ =>
+      seq('if', $.expression, optional(seq('else', $.expression))),
+    block_statement: $ => seq('block', $.identifier),
+    filter_statement: $ => seq('filter', $.expression),
+    macro_statement: $ => seq('macro', $.function_call),
+    extends_statement: $ =>
+      seq(
+        'extends',
+        choice($.string_literal, $.identifier),
+        $.inline_if_expression,
+      ),
+    call_statement: $ => seq('call', '(', $.identifier, ')', $.function_call),
+    with_statement: $ => seq('with', repeat($.assignment_expression)),
+    import_statement: $ =>
+      seq(
+        optional($.import_from),
+        'import',
+        $.identifier,
+        optional($.import_as),
+        optional($.import_attribute),
+      ),
+    import_from: $ => seq('from', $.string_literal),
+    import_as: $ => seq('as', commaSep1($.identifier)),
+    import_attribute: $ => $.attribute_context,
+    include_statement: $ =>
+      seq('include', $.string_literal, repeat($.include_attribute)),
+    include_attribute: $ =>
+      seq(optional('ignore missing'), $.attribute_context),
+    attribute_context: $ => seq(choice('with', 'without'), 'context'),
+    set_statement: $ =>
+      seq('set', commaSep1($.field_expression), '=', $.expression),
+    for_expression: $ =>
+      seq('for', $.identifier, 'in', $.expression, optional('recursive')),
     if_expression: $ => seq('if', $.expression),
 
     expression: $ => $.binary_expr,
     binary_expr: $ =>
       choice($.unary_expr, seq($.binary_expr, $.binary_operator, $.unary_expr)),
+    assignment_expression: $ => seq($.identifier, '=', $.expression),
     binary_operator: $ =>
       choice(
-        'is',
-        'and',
-        '>',
-        '<',
-        '=',
-        '!=',
-        '|',
-        '~',
         '+',
         '-',
         '*',
         '/',
         '//',
         '%',
+        '**',
+        '==',
+        '!=',
+        '>',
+        '>=',
+        '<',
+        '<=',
+        'and',
+        'or',
+        'is',
+        '|',
+        '~',
+        'in',
       ),
     unary_expr: $ =>
       choice($.primary_expr, seq($.unary_operator, $.unary_expr)),
@@ -71,6 +141,7 @@ module.exports = grammar({
         $.loop_expression,
         $.function_call,
         $.literal,
+        $.inline_trans,
         seq('(', $.expression, ')'),
       ),
     literal: $ =>
@@ -84,7 +155,7 @@ module.exports = grammar({
         $.dict_literal,
       ),
     boolean_literal: $ => choice('true', 'false'),
-    null_literal: $ => 'null',
+    null_literal: $ => choice('null', 'none'),
     number_literal: $ =>
       seq(
         optional(choice('-', '+')),
@@ -106,10 +177,16 @@ module.exports = grammar({
     list_literal: $ => seq('[', commaSep($.literal), ']'),
     dict_literal: $ =>
       seq('{', commaSep(seq($.string_literal, ':', $.literal)), '}'),
-    string_literal: $ => seq('"', repeat(choice('\\"', /[^\"]/)), '"'),
+    string_literal: $ =>
+      choice(
+        seq('"', repeat(choice('\\"', /[^\"]/)), '"'),
+        seq("'", repeat(choice(/[^']/, "\\'")), "'"),
+      ),
 
-    function_call: $ =>
-      seq($.field_expression, '(', commaSep($.field_expression), ')'),
+    inline_trans: $ => seq('_', '(', $.expression, ')'),
+
+    function_call: $ => seq($.field_expression, '(', commaSep($.arg), ')'),
+    arg: $ => seq(optional(seq($.identifier, '=')), $.expression),
 
     loop_expression: $ =>
       seq(
