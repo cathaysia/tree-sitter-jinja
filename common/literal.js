84583676fd01ac80ec01f8e05d0d1ff302d822cc
loongtao.zhang <loongtao.zhang@outlook.com>
2024-08-10 09:53:32 +0800

    feat(literal): better literal
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- common/literal.js
+++ common/literal.js
@@ -24,19 +24,26 @@ exports.rules = {
         $.big_dec_literal,
       ),
     ),
-  dec_literal: $ => choice('0', /[1-9][\d_]*/),
+  dec_literal: $ => /\d[\d_]*/,
   big_dec_literal: $ =>
     seq($.dec_literal, choice('e', 'e+', 'e-'), optional('_'), $.dec_literal),
-  oct_literal: $ => seq('0', 'o', $.dec_literal),
-  bin_literal: $ => seq('0', 'b', $.dec_literal),
-  hex_literal: $ => seq('0', 'x', /[\da-fA-F][\da-fA-F_]*/),
+  oct_literal: $ => /0o[0-7_]+/,
+  bin_literal: $ => /0b[01_]+/,
+  hex_literal: $ => /0x[\da-fA-F_]+/,
   float_literal: $ => seq($.dec_literal, '.', $.dec_literal),
-  list_literal: $ => seq('[', commaSep($.literal), ']'),
+
+  list_literal: $ => seq('[', commaSep($.expression), optional(','), ']'),
   dict_literal: $ =>
-    seq('{', commaSep(seq($.string_literal, ':', $.literal)), '}'),
+    seq(
+      '{',
+      commaSep(seq($.expression, ':', $.expression)),
+      optional(','),
+      '}',
+    ),
+
   string_literal: $ =>
     choice(
-      seq('"', repeat(choice('\\"', /[^\"]/)), '"'),
+      seq('"', repeat(choice(/[^\"]/, '\\"')), '"'),
       seq("'", repeat(choice(/[^']/, "\\'")), "'"),
     ),
 }
