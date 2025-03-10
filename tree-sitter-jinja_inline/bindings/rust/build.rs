f917bb198d13a4ff049d4ab40a7425e1b3c4af5b
loongtao.zhang <loongtao.zhang@outlook.com>
2025-03-10 10:35:56 +0800

    fix(rs): fix rust publish
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- tree-sitter-jinja_inline/bindings/rust/build.rs
+++ tree-sitter-jinja_inline/bindings/rust/build.rs
@@ -7,16 +7,15 @@ fn main() {
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
 
-    let parser_path = src_dir.join("parser.c").join("scanner.c");
+    let parser_path = src_dir.join("parser.c");
     c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
     // NOTE: if your language uses an external scanner, uncomment this block:
-    /*
+
     let scanner_path = src_dir.join("scanner.c");
     c_config.file(&scanner_path);
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
-    */
 
     c_config.compile("tree-sitter-jinja_inline");
 }
