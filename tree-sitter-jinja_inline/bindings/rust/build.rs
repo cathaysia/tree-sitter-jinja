501b3e3fee62cf025b8dbb2af024e1fda8121c74
loongtao.zhang <loongtao.zhang@outlook.com>
2025-03-10 10:28:44 +0800

    fix: fix symbol problem
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- tree-sitter-jinja_inline/bindings/rust/build.rs
+++ tree-sitter-jinja_inline/bindings/rust/build.rs
@@ -7,7 +7,7 @@ fn main() {
     #[cfg(target_env = "msvc")]
     c_config.flag("-utf-8");
 
-    let parser_path = src_dir.join("parser.c");
+    let parser_path = src_dir.join("parser.c").join("scanner.c");
     c_config.file(&parser_path);
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
